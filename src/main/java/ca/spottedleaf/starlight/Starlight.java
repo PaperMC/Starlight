a1fec6c2ea58ea92b62fe9eb81b3f15341ab40f5
Spottedleaf <spottedleaf@spottedleaf.dev>
2021-01-16 10:43:57 -0800

    Don't display incompatibility for FML server
    Starlight works whether only the client has it, only the
    server has it, or both have it.

--- src/main/java/ca/spottedleaf/starlight/Starlight.java
+++ src/main/java/ca/spottedleaf/starlight/Starlight.java
@@ -1,6 +1,17 @@
 package ca.spottedleaf.starlight;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("starlight")
-public final class Starlight {}
+public final class Starlight {
+
+    public Starlight() {
+        // starlight is network compatible both ways
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+
+}
