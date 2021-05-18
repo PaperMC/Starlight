cdb04e3a5f301305692daa80b569a31cbb51eb85
Aeiou <3160746+AeiouEnigma@users.noreply.github.com>
2021-05-18 17:29:42 -0400

    Move VariableBlockLightHandler methods from WrappedWorldMixin to WorldMixin Add default, null-returning implementations of getChunkAtImmediately and getAnyChunkImmediately to ExtendedWorld

--- src/main/java/ca/spottedleaf/starlight/common/world/ExtendedWorld.java
+++ src/main/java/ca/spottedleaf/starlight/common/world/ExtendedWorld.java
@@ -7,10 +7,14 @@ import net.minecraft.world.chunk.IChunk;
 public interface ExtendedWorld {
 
     // rets full chunk without blocking
-    public Chunk getChunkAtImmediately(final int chunkX, final int chunkZ);
+    default public Chunk getChunkAtImmediately(final int chunkX, final int chunkZ) {
+        return null;
+    }
 
     // rets chunk at any stage, if it exists, immediately
-    public IChunk getAnyChunkImmediately(final int chunkX, final int chunkZ);
+    default public IChunk getAnyChunkImmediately(final int chunkX, final int chunkZ) {
+        return null;
+    }
 
     public VariableBlockLightHandler getCustomLightHandler();
 
--- src/main/java/ca/spottedleaf/starlight/mixin/common/world/WorldMixin.java
+++ src/main/java/ca/spottedleaf/starlight/mixin/common/world/WorldMixin.java
@@ -1,9 +1,28 @@
 package ca.spottedleaf.starlight.mixin.common.world;
 
+import ca.spottedleaf.starlight.common.light.VariableBlockLightHandler;
 import ca.spottedleaf.starlight.common.world.ExtendedWorld;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunk;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(World.class)
-public abstract class WorldMixin implements IWorld, AutoCloseable, ExtendedWorld {}
+public abstract class WorldMixin implements IWorld, AutoCloseable, ExtendedWorld {
+
+    @Unique
+    private VariableBlockLightHandler customBlockLightHandler;
+
+    @Override
+    public VariableBlockLightHandler getCustomLightHandler() {
+        return this.customBlockLightHandler;
+    }
+
+    @Override
+    public void setCustomLightHandler(final VariableBlockLightHandler handler) {
+        this.customBlockLightHandler = handler;
+    }
+
+}
