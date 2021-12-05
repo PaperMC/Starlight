fe9d47e002422714f63d76c43a75a5c12017ac9d
Noah van der Aa <ndvdaa@gmail.com>
2021-12-05 11:30:32 +0100

    I 100% blame IntellIJ for this

--- src/main/java/ca/spottedleaf/starlight/common/light/StarLightInterface.java
+++ src/main/java/ca/spottedleaf/starlight/common/light/StarLightInterface.java
@@ -271,7 +271,7 @@ public final class StarLightInterface {
         final int sky = this.getSkyLightValue(pos, chunk) - ambientDarkness;
         // Don't fetch the block light level if the skylight level is 15, since the value will never be higher.
         if (sky == 15) {
-			return 15;
+            return 15;
         }
         final int block = this.getBlockLightValue(pos, chunk);
         return Math.max(sky, block);
