dfa2fd070d5b3334acfcc2c6357b3b47345fabc2
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-07-18 02:30:21 +0200

    fix #187
    add a default implementation for getChunkAtImmediately and getAnyChunkImmediately

--- src/main/java/ca/spottedleaf/starlight/common/world/ExtendedWorld.java
+++ src/main/java/ca/spottedleaf/starlight/common/world/ExtendedWorld.java
@@ -6,9 +6,13 @@ import net.minecraft.world.level.chunk.LevelChunk;
 public interface ExtendedWorld {
 
     // rets full chunk without blocking
-    public LevelChunk getChunkAtImmediately(final int chunkX, final int chunkZ);
+    default LevelChunk getChunkAtImmediately(final int chunkX, final int chunkZ) {
+        return null;
+    }
 
     // rets chunk at any stage, if it exists, immediately
-    public ChunkAccess getAnyChunkImmediately(final int chunkX, final int chunkZ);
+    default ChunkAccess getAnyChunkImmediately(final int chunkX, final int chunkZ) {
+        return null;
+    }
 
 }
