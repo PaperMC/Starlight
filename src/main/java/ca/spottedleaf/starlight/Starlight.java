ebf7f9f0fc83c3d7f1095b5dedacccaef981a6df
Spottedleaf <Spottedleaf@users.noreply.github.com>
2023-10-14 17:38:24 -0700

    Update to 1.20.2

--- src/main/java/ca/spottedleaf/starlight/Starlight.java
+++ src/main/java/ca/spottedleaf/starlight/Starlight.java
@@ -3,7 +3,6 @@ package ca.spottedleaf.starlight;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod("starlight")
 public final class Starlight {
@@ -11,7 +10,7 @@ public final class Starlight {
     public Starlight() {
         // starlight is network compatible both ways
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> {
-            return new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true);
+            return new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true);
         });
     }
 
