5867eae3b82abe3ef1387e60bc86752d9cda63fb
Spottedleaf <Spottedleaf@users.noreply.github.com>
2023-10-16 15:51:31 -0700

    Initialise default nibble arrays in ChunkAccess directly
    When ChunkAccess was converted to an abstract class some versions
    ago, the code to initialise nibble arrays should have been moved.
    However, the code was not moved and so now mods constructing their
    own implementations of ChunkAccess would not have the nibble arrays
    initialised.
    
    Fixes https://github.com/PaperMC/Starlight/issues/186

--- src/main/java/ca/spottedleaf/starlight/mixin/common/chunk/ProtoChunkMixin.java
+++ src/main/java/ca/spottedleaf/starlight/mixin/common/chunk/ProtoChunkMixin.java
@@ -1,42 +1,15 @@
 package ca.spottedleaf.starlight.mixin.common.chunk;
 
-import ca.spottedleaf.starlight.common.light.StarLightEngine;
 import ca.spottedleaf.starlight.common.chunk.ExtendedChunk;
-import net.minecraft.core.Registry;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.LevelHeightAccessor;
-import net.minecraft.world.level.chunk.ImposterProtoChunk;
-import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.chunk.ProtoChunk;
-import net.minecraft.world.level.chunk.UpgradeData;
-import net.minecraft.world.level.levelgen.blending.BlendingData;
 import net.minecraft.world.level.lighting.ChunkSkyLightSources;
-import net.minecraft.world.ticks.ProtoChunkTicks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ProtoChunk.class)
 public abstract class ProtoChunkMixin implements ExtendedChunk {
-
-    /**
-     * Initialises the nibble arrays to default values.
-     * TODO since this is a constructor inject, check for new constructors on update.
-     */
-    @Inject(
-            method = "<init>(Lnet/minecraft/world/level/ChunkPos;Lnet/minecraft/world/level/chunk/UpgradeData;[Lnet/minecraft/world/level/chunk/LevelChunkSection;Lnet/minecraft/world/ticks/ProtoChunkTicks;Lnet/minecraft/world/ticks/ProtoChunkTicks;Lnet/minecraft/world/level/LevelHeightAccessor;Lnet/minecraft/core/Registry;Lnet/minecraft/world/level/levelgen/blending/BlendingData;)V",
-            at = @At("TAIL")
-    )
-    public void onConstruct(ChunkPos chunkPos, UpgradeData upgradeData, LevelChunkSection[] levelChunkSections, ProtoChunkTicks protoChunkTicks, ProtoChunkTicks protoChunkTicks2, LevelHeightAccessor levelHeightAccessor, Registry registry, BlendingData blendingData, CallbackInfo ci) {
-        if ((Object)this instanceof ImposterProtoChunk) {
-            return;
-        }
-        this.setBlockNibbles(StarLightEngine.getFilledEmptyLight(levelHeightAccessor));
-        this.setSkyNibbles(StarLightEngine.getFilledEmptyLight(levelHeightAccessor));
-    }
 
     /**
      * @reason Remove unused skylight sources
